d605de41c606b77836c2df14bd766503dfb1f55a
Mark Sayers <marksayers46@icloud.com>
2020-07-23 18:44:58 -0500

    problem with npm package manager

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { initialState, todoReducer } from './reducers/reducer';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
-  return (
-    <div className="todo-list">
+  const [state, dispatch] = useReducer(todoReducer, initialState);
 
+  const clearCompleted = () => {
+    dispatch({type:'clearCompleted', payload: state})
+  }
+
+  return (
+    <div className='app'>
+      <TodoForm
+        state={state}
+        dispatch={dispatch}
+      />
+      <TodoList 
+        state={state}
+        dispatch={dispatch}
+      />
+      <button onClick={clearCompleted}>Clear Completed Task</button>
     </div>
   );
 }
---
+++ todo/src/components/item.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import moment from 'moment';
+
+const Item = props => {
+    let itemClassName = 'item';
+
+    if (props.item.completed) {
+        itemClassName = itemClassName + 'completed';
+    }
+
+    const handleClick = e => {
+        props.dispatch({type: 'toggleCompleted', payload: props.item.id});
+    }
+    
+    return(
+        <div>
+            <p>
+                <span className={itemClassName} onClick={handleClick} value={props.item.id}>{props.item.item} </span>
+                {props.item.completed === true ?  <span className='time'> Completed {moment().calendar()}</span> : null}
+            </p>
+        </div>
+    )
+}
+
+export default Item;
