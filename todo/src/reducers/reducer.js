62d1c02663531cb0d093192db96372bff0ea3273
Mark Sayers <marksayers46@icloud.com>
2020-07-27 12:40:58 -0500

    modified reducer.js

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,29 +1,29 @@
 export const initialState = [
-  {
-  item: 'Learn about reducers',
-  completed: false,
-  id: 3892987589
-  }
+    {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+    }
 ]
 
 export const todoReducer = (state, action) => {
-  switch (action.type) {
-      case 'addToList': 
-          return [...state,
-              {
-                  item: action.payload,
-                  completed: false,
-                  id: Date.now()
-              }
-          ];
-      case 'toggleCompleted':         
-          return state.map(item => 
-              item.id === action.payload ? {...item, completed: !item.completed} : item
-          );
-      case 'clearCompleted':
-          return state.filter(item => 
-              !item.completed
-          );
-      default: return state;
-  }    
+    switch (action.type) {
+        case 'addToList': 
+            return [...state,
+                {
+                    item: action.payload,
+                    completed: false,
+                    id: Date.now()
+                }
+            ];
+        case 'toggleCompleted':         
+            return state.map(item => 
+                item.id === action.payload ? {...item, completed: !item.completed} : item
+            );
+        case 'clearCompleted':
+            return state.filter(item => 
+                !item.completed
+            );
+        default: return state;
+    }    
 }
